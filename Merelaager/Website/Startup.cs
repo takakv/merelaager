ef77cf5569c78ea728dd3cbac99965cc41bb3c33
Taka <71265675+takakv@users.noreply.github.com>
2020-11-16 21:00:14 +0200

    Reset dotnet version

--- Merelaager/Website/Startup.cs
+++ Merelaager/Website/Startup.cs
@@ -30,11 +30,10 @@ namespace Website
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +42,7 @@ namespace Website
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseMigrationsEndPoint();
+                app.UseDatabaseErrorPage();
             }
             else
             {
