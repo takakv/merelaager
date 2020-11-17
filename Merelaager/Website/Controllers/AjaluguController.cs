fc0bb7c0389c6c9c7b6337cc113eeeabf396f647
Taka <71265675+takakv@users.noreply.github.com>
2020-11-17 13:42:43 +0200

    Port static website to asp.net (#2)

---
+++ Merelaager/Website/Controllers/AjaluguController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class AjaluguController : Controller
+    {
+        private readonly ILogger<AjaluguController> _logger;
+
+        public AjaluguController(ILogger<AjaluguController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
