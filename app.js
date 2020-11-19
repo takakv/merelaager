a18fb0a545f9a19a920e3b9cfb6fc0003014fc73
Taka <71265675+takakv@users.noreply.github.com>
2020-11-19 17:18:34 +0200

    Update head template

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-const handlebars = require('express-handlebars');
+const handlebars = require("express-handlebars");
+const fs = require("fs");
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
-app.engine('hbs', handlebars({
-  extname: 'hbs',
-  defaultView: 'default',
-  layoutsDir: __dirname + '/views/layouts/',
-  partialsDir: __dirname + '/views/partials/'
-}));
+app.engine(
+  "hbs",
+  handlebars({
+    extname: "hbs",
+    defaultView: "default",
+    layoutsDir: __dirname + "/views/layouts/",
+    partialsDir: __dirname + "/views/partials/",
+  })
+);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res, next) => {
-  res.render('index', {layout : 'default'});
+let meta = JSON.parse(fs.readFileSync("./data/metadata.json", "utf-8"));
+
+app.get("/", (req, res, next) => {
+  res.render("index", {
+    layout: "default",
+    title: meta.homepage.title,
+    description: meta.homepage.description,
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
