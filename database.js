d4a895f5e539d0ad10e6fd60cacf579c09811423
Taka <71265675+takakv@users.noreply.github.com>
2020-11-20 22:48:07 +0200

    Reswitch to sequelize

--- database.js
+++ database.js
@@ -1,5 +1,4 @@
 const { Sequelize } = require("sequelize");
-const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -8,25 +7,25 @@ const user = process.env.MYSQL_USER;
 const pass = process.env.MYSQL_PWD;
 const dbname = process.env.MYSQL_DB;
 
-const dbCon = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PWD,
-    database: process.env.MYSQL_DB,
-});
+// const dbCon = mysql.createConnection({
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PWD,
+//     database: process.env.MYSQL_DB,
+// });
+//
+// dbCon.connect((err) => {
+//     if (err) throw err;
+//     console.log("Connected");
+// });
 
-dbCon.connect((err) => {
-    if (err) throw err;
-    console.log("Connected");
-});
+const sequelize = new Sequelize(
+  `mysql://${user}:${pass}@${host}:3306/${dbname}`
+);
 
-// const sequelize = new Sequelize(
-//   `mysql://${user}:${pass}@${host}:3306/${dbname}`
-// );
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connection successful."))
+  .catch((err) => console.error("Database connection failed:", err));
 
-// sequelize
-//   .authenticate()
-//   .then(() => console.log("Database connection successful."))
-//   .catch((err) => console.error("Database connection failed:", err));
-
-// module.exports = sequelize;
+module.exports = sequelize;
