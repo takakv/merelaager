91b66e96f4bd2f9d2845ffc1417da1169fd284d3
Taka <71265675+takakv@users.noreply.github.com>
2020-11-20 22:00:35 +0200

    Revert to mysql

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-
+const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -8,13 +8,25 @@ const user = process.env.MYSQL_USER;
 const pass = process.env.MYSQL_PWD;
 const dbname = process.env.MYSQL_DB;
 
-const sequelize = new Sequelize(
-  `mysql://${user}:${pass}@${host}:3306/${dbname}`
-);
+const dbCon = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DB,
+});
 
-sequelize
-  .authenticate()
-  .then(() => console.log("Database connection successful."))
-  .catch((err) => console.error("Database connection failed:", err));
+dbCon.connect((err) => {
+    if (err) throw err;
+    console.log("Connected");
+});
 
-module.exports = sequelize;
+// const sequelize = new Sequelize(
+//   `mysql://${user}:${pass}@${host}:3306/${dbname}`
+// );
+
+// sequelize
+//   .authenticate()
+//   .then(() => console.log("Database connection successful."))
+//   .catch((err) => console.error("Database connection failed:", err));
+
+// module.exports = sequelize;
