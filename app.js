a56bdc57d714fa87674de7c795f4bbd045345a32
Taka <71265675+takakv@users.noreply.github.com>
2020-11-29 21:06:19 +0200

    Update prices

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const slashes = require("connect-slashes");
 const app = express();
 
 const dotenv = require("dotenv");
@@ -8,60 +9,60 @@ const exphbs = require("express-handlebars");
 const fs = require("fs");
 
 const hbs = exphbs.create({
-    extname: "hbs",
-    defaultView: "index",
-    defaultLayout: "default",
-    layoutsDir: __dirname + "/views/layouts/",
-    partialsDir: __dirname + "/views/partials/",
-    helpers: {
-        times: (n, block) => {
-            let accum = ""
-            for (let i = 1; i <= 4; ++i) {
-                accum += block.fn(i);
-            }
-            return accum;
-        }
-    }
-})
+  extname: "hbs",
+  defaultView: "index",
+  defaultLayout: "default",
+  layoutsDir: __dirname + "/views/layouts/",
+  partialsDir: __dirname + "/views/partials/",
+  helpers: {
+    times: (n, block) => {
+      let accum = "";
+      for (let i = 1; i <= 4; ++i) {
+        accum += block.fn(i);
+      }
+      return accum;
+    },
+  },
+});
 
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 
-app.use(express.static("public"));
+app.use(express.static("public")).use(slashes());
 
 let meta = JSON.parse(fs.readFileSync("./data/metadata.json", "utf-8"));
 
 app.get("/", (req, res, next) => {
-    res.render("index", {
-        layout: "landing",
-        title: meta.homepage.title,
-        description: meta.homepage.description,
-        body_class: "landing",
-    });
+  res.render("index", {
+    layout: "landing",
+    title: meta.homepage.title,
+    description: meta.homepage.description,
+    body_class: "landing",
+  });
 });
 
 app.get("/meeskond/", (req, res, next) => {
-    res.render("meeskond", {
-        title: meta.meeskond.title,
-        description: meta.meeskond.description,
-        body_class: "meeskond",
-    });
+  res.render("meeskond", {
+    title: meta.meeskond.title,
+    description: meta.meeskond.description,
+    body_class: "meeskond",
+  });
 });
 
 app.get("/ajalugu/", (req, res, next) => {
-    res.render("ajalugu", {
-        title: meta.ajalugu.title,
-        description: meta.ajalugu.description,
-        body_class: "ajalugu",
-    });
+  res.render("ajalugu", {
+    title: meta.ajalugu.title,
+    description: meta.ajalugu.description,
+    body_class: "ajalugu",
+  });
 });
 
 app.get("/pildid/", (req, res, next) => {
-    res.render("pildid", {
-        title: meta.pildid.title,
-        description: meta.pildid.description,
-        body_class: "pildid",
-    });
+  res.render("pildid", {
+    title: meta.pildid.title,
+    description: meta.pildid.description,
+    body_class: "pildid",
+  });
 });
 
 const infoRouter = require("./routes/info");
@@ -73,8 +74,12 @@ app.use("/kambuus/", adminRouter);
 const registerRouter = require("./routes/register");
 app.use("/registreerimine/", registerRouter);
 
+app.get("/broneerimine/", (req, res, next) => {
+  res.redirect("/registreerimine/");
+});
+
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 const db = require("./models/database");
-db.sequelize.sync({alter: true});
+db.sequelize.sync({ alter: true });
