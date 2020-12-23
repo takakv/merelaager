5141c3969cbc3f6bc448684305b26dcb171302c7
Taka <71265675+takakv@users.noreply.github.com>
2020-12-23 17:44:50 +0200

    Add testing

--- controllers/camperController.js
+++ controllers/camperController.js
@@ -78,15 +78,7 @@ const auth = {
 };
 
 const mailer = async () => {
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: process.env.EMAIL_HOST,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PWD,
-    },
-  });
+  const transporter = nodemailer.createTransport(mailGun(auth));
 
   const meta = {
     from: "bronn@merelaager.ee",
