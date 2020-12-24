639e37bddaec660a55d3f12f19ff4a9175be1494
Taka <71265675+takakv@users.noreply.github.com>
2020-12-24 05:26:09 +0200

    Update host

--- controllers/camperController.js
+++ controllers/camperController.js
@@ -74,8 +74,8 @@ const auth = {
   auth: {
     api_key: process.env.EMAIL_API,
     domain: process.env.EMAIL_SERV,
-    host: "api.eu.mailgun.net",
   },
+  host: "api.eu.mailgun.net",
 };
 
 const mailer = async () => {
