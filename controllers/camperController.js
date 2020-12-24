d45403c0807278f45ca69bd874917b13116568ea
Taka <71265675+takakv@users.noreply.github.com>
2020-12-24 05:49:17 +0200

    Add shift data

--- controllers/camperController.js
+++ controllers/camperController.js
@@ -82,7 +82,7 @@ const auth = {
 };
 
 const mailer = async (shift) => {
-  console.log(shiftData);
+  console.log(shiftData[`${shift}`]);
   const transporter = nodemailer.createTransport(mailGun(auth));
 
   const meta = {
