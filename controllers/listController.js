affcdaedc4e24f78d316ee4c7f1b2aeca6d7f162
Taka <71265675+takakv@users.noreply.github.com>
2020-12-30 14:55:39 +0200

    Update campers display

--- controllers/listController.js
+++ controllers/listController.js
@@ -6,9 +6,9 @@ const Camper = db.campers;
 exports.generate = async (req, res) => {
   if (req.body["password"] !== process.env.LISTPASS) return false;
   const children = await Camper.findAll({
-    where: {
-      vahetus: "3v",
-    },
+    // where: {
+    //   vahetus: "3v",
+    // },
     order: [["arveNr", "ASC"]],
   });
   const childData = [];
@@ -16,11 +16,13 @@ exports.generate = async (req, res) => {
     const data = {
       name: child["nimi"],
       gender: child["sugu"],
+      idCode: child["isikukood"],
       bDay: child["synnipaev"],
       isOld: child["vana_olija"] ? "jah" : "ei",
       shift: child["vahetus"],
       tShirtSize: child["ts_suurus"],
       city: child["linn"],
+      county: child["maakond"],
       billNr: child["arveNr"],
       contact: `${child["kontakt_nimi"]}, ${child["kontakt_email"]}, ${child["kontakt_number"]}`,
       registered: child["registreeritud"] ? "jah" : "ei",
