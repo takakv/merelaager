0cde08e3c838e20374f5df4721d43d091f1a0af0
Taka <71265675+takakv@users.noreply.github.com>
2021-01-01 12:56:32 +0200

    Update unlock time

--- public/media/scripts/registration.js
+++ public/media/scripts/registration.js
@@ -86,9 +86,9 @@ const unlocker = (moment) => {
 };
 
 if (window.location.hostname === "merelaager.ee") {
-  unlocker("01 Jan 2021 14:00:00 UTC");
+  unlocker("01 Jan 2021 12:00:00 UTC");
 } else {
-  unlocker("27 Dec 2020 08:56:00 UTC");
+  unlocker("01 Jan 2021 10:58:00 UTC");
 }
 
 const source = new EventSource("/registreerimine/events/");
