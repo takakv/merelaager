3fc25f76403727fc47918b8a5a524ef58943b01e
Taka <71265675+takakv@users.noreply.github.com>
2021-01-20 11:07:34 +0200

    Allow removing children from tents

--- public/media/scripts/camperTents.js
+++ public/media/scripts/camperTents.js
@@ -1,7 +1,7 @@
-document.addEventListener("change", (event) => {
+const updateTent = (id, tent) => {
   const data = {
-    id: event.target.parentElement.id,
-    tent: event.target.value,
+    id: id,
+    tent: tent,
   };
   fetch(`${window.location.href}update/tent/`, {
     method: "POST",
@@ -10,4 +10,15 @@ document.addEventListener("change", (event) => {
     },
     body: JSON.stringify(data),
   }).catch((err) => alert(err));
+};
+
+document.addEventListener("change", (event) => {
+  updateTent(event.target.parentElement.id, event.target.value);
+});
+
+document.addEventListener("click", (event) => {
+  if (!event.target.classList.contains("c-tent-child__rm")) return;
+  let id = event.target.parentElement.id;
+  id = id.split("-")[0];
+  updateTent(id, null);
 });
