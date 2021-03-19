f35181da5f4634cc7bd01e2fbd9cd0fc1dd00878
Taka <71265675+takakv@users.noreply.github.com>
2021-03-19 03:48:51 +0200

    Separate registration list concerns

--- src/components/RegList/RegList.js
+++ src/components/RegList/RegList.js
@@ -1,38 +1,7 @@
 import React, { Component, Suspense } from "react";
 import campers from "./fetchCampers";
-
-class ListHead extends Component {
-  render() {
-    return (
-      <div id={this.props.id} className="c-cl-head">
-        <div className="c-cl-head__group c-cl-head__group-main">
-          <div className="c-cl-count">
-            <span className="head">poisid: </span>
-            {this.props.shiftData.regBoyCount}
-          </div>
-          <div className="c-cl-count">
-            <span className="head">tüdrukud: </span>
-            {this.props.shiftData.regGirlCount}
-          </div>
-          <div className="c-cl-count">
-            <span className="head">kokku: </span>
-            {this.props.shiftData.totalCount}
-          </div>
-        </div>
-        <div className="c-cl-head__group">
-          <div className="c-cl-count">
-            <span className="head">res. poisid: </span>
-            {this.props.shiftData.resBoyCount}
-          </div>
-          <div className="c-cl-count">
-            <span className="head">res. tüdrukud: </span>
-            {this.props.shiftData.resGirlCount}
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+import { ListHead } from "./Support/List";
+import { Table } from "./Support/Table";
 
 const Display = () => {
   const shiftDataList = Object.entries(campers());
@@ -48,87 +17,15 @@ const Display = () => {
       {shifts.map((shift) => (
         <button id={`${shift}-switch`}>{shift}</button>
       ))}
-      {shiftDataList.map((shift) => (
+      {shiftDataList.map((shift, index) => (
         <ListHead id={`${shift[0]}v-head`} shiftData={shift[1]} />
       ))}
       {shiftDataList.map((shift, index) => (
-        <table id={`${++index}v-table`} className="c-camper-table">
-          <thead>
-            <tr>
-              <th>Reg ID</th>
-              <th>Reg?</th>
-              <th>Nimi</th>
-              <th>Makstud</th>
-              <th>Kogusumma</th>
-              <th>Kontakt</th>
-              <th>Vana?</th>
-              <th>Sünnipäev</th>
-              <th>Ts</th>
-              <th>Tln?</th>
-              <th>Arve nr</th>
-              <th>Isikukood</th>
-            </tr>
-          </thead>
-          {listSections.map((section) => (
-            <tbody>
-              <tr>
-                <td colSpan="14">{section[0]}</td>
-              </tr>
-              {shift[1][section[1]].map((kid) => (
-                <tr>
-                  <td>{kid.id}</td>
-                  <td>
-                    <button
-                      id={`${kid.id}-reg`}
-                      className={`state ${kid.registered} clicker`}
-                    />
-                  </td>
-                  <td>{kid.name}</td>
-                  <td>
-                    <input
-                      id={`${kid.id}-paid`}
-                      className="price"
-                      type="text"
-                      defaultValue={kid.pricePaid}
-                    />
-                  </td>
-                  <td>
-                    <input
-                      id={`${kid.id}-toPay`}
-                      className="priceToPay"
-                      type="text"
-                      defaultValue={kid.priceToPay}
-                    />
-                  </td>
-                  <td id={`${kid.id}-contact`} className="c-camper-contact">
-                    <a
-                      href={`mailto:${kid.contactEmail}`}
-                      className="c-caper-contact__name"
-                    >
-                      {kid.contactName}
-                    </a>
-                    <span className="c-camper-contact__phone">
-                      {kid.contactNumber}
-                    </span>
-                  </td>
-                  <td>
-                    <button
-                      id={`${kid.id}-old`}
-                      className={`state ${kid.isOld} clicker`}
-                    >
-                      {kid.isOld}
-                    </button>
-                  </td>
-                  <td style={{ fontFamily: "monospace" }}>{kid.bDay}</td>
-                  <td>{kid.tShirtSize}</td>
-                  <td>{kid.tln}</td>
-                  <td style={{ fontFamily: "monospace" }}>{kid.billNr}</td>
-                  <td style={{ fontFamily: "monospace" }}>{kid.idCode}</td>
-                </tr>
-              ))}
-            </tbody>
-          ))}
-        </table>
+        <Table
+          listSections={listSections}
+          shiftData={shift[1]}
+          tableId={`${++index}v-table`}
+        />
       ))}
     </div>
   );
---
+++ src/components/RegList/Support/Table.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+
+class TableHeader extends Component {
+  render() {
+    return (
+      <thead>
+        <tr>
+          <th>Reg ID</th>
+          <th>Reg?</th>
+          <th>Nimi</th>
+          <th>Makstud</th>
+          <th>Kogusumma</th>
+          <th>Kontakt</th>
+          <th>Vana?</th>
+          <th>Sünnipäev</th>
+          <th>Ts</th>
+          <th>Tln?</th>
+          <th>Arve nr</th>
+          <th>Isikukood</th>
+        </tr>
+      </thead>
+    );
+  }
+}
+
+class TableSection extends Component {
+  render() {
+    return (
+      <tbody>
+        <tr>
+          <td colSpan="14">{this.props.title}</td>
+        </tr>
+        {this.props.sectionData.map((kid) => (
+          <tr>
+            <td>{kid.id}</td>
+            <td>
+              <button
+                id={`${kid.id}-reg`}
+                className={`state ${kid.registered} clicker`}
+              />
+            </td>
+            <td>{kid.name}</td>
+            <td>
+              <input
+                id={`${kid.id}-paid`}
+                className="price"
+                type="text"
+                defaultValue={kid.pricePaid}
+              />
+            </td>
+            <td>
+              <input
+                id={`${kid.id}-toPay`}
+                className="priceToPay"
+                type="text"
+                defaultValue={kid.priceToPay}
+              />
+            </td>
+            <td id={`${kid.id}-contact`} className="c-camper-contact">
+              <a
+                href={`mailto:${kid.contactEmail}`}
+                className="c-caper-contact__name"
+              >
+                {kid.contactName}
+              </a>
+              <span className="c-camper-contact__phone">
+                {kid.contactNumber}
+              </span>
+            </td>
+            <td>
+              <button
+                id={`${kid.id}-old`}
+                className={`state ${kid.isOld} clicker`}
+              >
+                {kid.isOld}
+              </button>
+            </td>
+            <td style={{ fontFamily: "monospace" }}>{kid.bDay}</td>
+            <td>{kid.tShirtSize}</td>
+            <td>{kid.tln}</td>
+            <td style={{ fontFamily: "monospace" }}>{kid.billNr}</td>
+            <td style={{ fontFamily: "monospace" }}>{kid.idCode}</td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
+}
+
+export class Table extends Component {
+  render() {
+    return (
+      <table id={this.props.tableId} className="c-camper-table">
+        <TableHeader />
+        {this.props.listSections.map((section) => (
+          <TableSection
+            title={section[0]}
+            sectionData={this.props.shiftData[section[1]]}
+          />
+        ))}
+      </table>
+    );
+  }
+}
