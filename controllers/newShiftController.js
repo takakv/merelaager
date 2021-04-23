b7b59eceb80bb350005679d279c474f263036ea5
Taka <71265675+takakv@users.noreply.github.com>
2021-04-23 04:35:02 +0300

    Associate registered campers with shifts

---
+++ controllers/newShiftController.js
@@ -0,0 +1,32 @@
+const db = require("../models/database");
+
+const Children = db.children;
+const Reglist = db.campers;
+const ShiftData = db.shiftData;
+
+exports.forceUpdate = async () => {
+  const campers = [];
+
+  // Fetch all registered campers.
+  const regCampers = await Reglist.findAll({
+    where: { isRegistered: true },
+  });
+
+  // Associate all registered campers with shifts.
+  regCampers.forEach(async (camper) => {
+    const nameStamp = camper.name.toLowerCase().replace(/\s/g, "");
+    const child = await Children.findOne({ where: { nameStamp: nameStamp } });
+    campers.push({
+      childId: child.id,
+      shiftNr: parseInt(camper.shift[0]),
+    });
+  });
+
+  // Update the shifts list.
+  campers.forEach((camper) => {
+    ShiftData.Create({
+      childId: camper.childId,
+      shiftNr: camper.shiftNr,
+    });
+  });
+};
--- routes/api.js
+++ routes/api.js
@@ -47,6 +47,7 @@ const bill = require("../controllers/billController");
 const shiftData = require("../controllers/shiftController");
 const shirtsData = require("../controllers/shirtController");
 const childData = require("../controllers/childController");
+const newShiftData = require("../controllers/newShiftController");
 
 // FORCED DB UPDATES.
 router.post("/ucl/", async (req, res) => {
@@ -66,6 +67,18 @@ router.post("/children/", async (req, res) => {
   if (req.body.token !== process.env.API_OVERRIDE) return res.sendStatus(403);
   try {
     await childData.forceUpdate();
+    res.sendStatus(200);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+});
+
+router.post("/shift/", async (req, res) => {
+  if (!("token" in req.body)) return res.sendStatus(401);
+  if (req.body.token !== process.env.API_OVERRIDE) return res.sendStatus(403);
+  try {
+    await newShiftData.forceUpdate();
     res.sendStatus(200);
   } catch (e) {
     console.error(e);
