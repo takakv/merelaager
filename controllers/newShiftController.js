aa46e1478121fc78c909f458dedcf2fa5520d1d4
Taka <71265675+takakv@users.noreply.github.com>
2021-04-23 04:52:39 +0300

    Fix shift data creation async issues

--- controllers/newShiftController.js
+++ controllers/newShiftController.js
@@ -5,28 +5,21 @@ const Reglist = db.campers;
 const ShiftData = db.shiftData;
 
 exports.forceUpdate = async () => {
-  const campers = [];
-
   // Fetch all registered campers.
   const regCampers = await Reglist.findAll({
     where: { isRegistered: true },
   });
 
   // Associate all registered campers with shifts.
-  regCampers.forEach(async (camper) => {
+  await regCampers.forEach((camper) => {
     const nameStamp = camper.name.toLowerCase().replace(/\s/g, "");
-    const child = await Children.findOne({ where: { nameStamp: nameStamp } });
-    campers.push({
-      childId: child.id,
-      shiftNr: parseInt(camper.shift[0]),
-    });
-  });
-
-  // Update the shifts list.
-  campers.forEach((camper) => {
-    ShiftData.create({
-      childId: camper.childId,
-      shiftNr: camper.shiftNr,
-    });
+    Children.findOne({ where: { nameStamp } })
+      .then((child) => {
+        ShiftData.create({
+          childId: child.id,
+          shiftNr: parseInt(camper.shift[0]),
+        });
+      })
+      .catch(console.error);
   });
 };
