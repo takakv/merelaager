9af88f29ff54ce76efa884126bc95398e067e368
Taka <71265675+takakv@users.noreply.github.com>
2021-12-28 15:35:26 +0100

    Fix namespace bugs

--- utilities/dummyGenerator/dummyGenerator.js
+++ utilities/dummyGenerator/dummyGenerator.js
@@ -1,84 +1,6 @@
 const fs = require("fs");
 const config = require("./configGetters");
-
-const separator = "&amp;";
-
-const tsSizes = ["118/128", "130/140", "142/152", "152/164", "M", "L", "XL"];
-
-const getRandomDate = (start, end) => {
-  return new Date(
-    start.getTime() + Math.random() * (end.getTime() - start.getTime())
-  );
-};
-
-const getIdCode = (gender) => {
-  let idCode = gender === "M" ? "5" : "6";
-  const bDay = getRandomDate(new Date(2004, 0, 1), new Date(2017, 11, 31));
-
-  const year = bDay.getFullYear().toString().substr(-2);
-  let month = bDay.getMonth() + 1;
-  month = month.toLocaleString("en-US", {
-    minimumIntegerDigits: 2,
-    useGrouping: false,
-  });
-  const day = bDay
-    .getDate()
-    .toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false });
-  idCode += `${year}${month}${day}1234`;
-  return idCode;
-};
-
-const getSingle = (name, shiftNr, gender) => {
-  let res = "";
-
-  const idCode = getIdCode(gender);
-  const tsSize = tsSizes[Math.floor(Math.random() * tsSizes.length)];
-
-  res += `name=${encodeURIComponent(name)}` + separator;
-  res += `idCode=${idCode}` + separator;
-  res += `isNew=${Math.round(Math.random())}` + separator;
-  res += `shiftNr=${shiftNr}` + separator;
-  res += `road=Merelaagri tee` + separator;
-  res += `city=${encodeURIComponent("Laoküla")}` + separator;
-  res += `county=Harju` + separator;
-  res += `tsSize=${tsSize}` + separator;
-  // res += `addendum:\n`;
-  // res += `country:Eesti\n`;
-
-  return res;
-};
-
-const getFirstName = (firstNames, gender) => {
-  const count = firstNames[gender].length;
-  return firstNames[gender][Math.floor(Math.random() * count)];
-};
-
-const getData = (names, simCount, lName, gender, shiftNr) => {
-  let res = "";
-  let name;
-
-  // for (let shiftNr = 1; shiftNr <= 5; ++shiftNr) {
-  for (let i = 0; i < simCount; ++i) {
-    name = `${getFirstName(names.first, gender)} ${lName}`;
-    res += getSingle(name, shiftNr, gender);
-  }
-  //}
-
-  return res;
-};
-
-const getMeta = (simCount, fNames, lName) => {
-  const gender = Math.round(Math.random()) === 1 ? "M" : "F";
-  const fName = getFirstName(fNames, gender);
-
-  let res = "";
-  res += `contactName=${encodeURIComponent(`${fName} ${lName}`)}` + separator;
-  res += `contactNumber=%2B372 xxx` + separator;
-  res += `contactEmail=${encodeURIComponent(`${fName}@${lName}`)}` + separator;
-  res += `childCount=${simCount}` + separator;
-  res += `noEmail=true`;
-  return res;
-};
+const dataGetters = require("./dataGetters");
 
 exports.generate = () => {
   const names = JSON.parse(fs.readFileSync("./data/names.json", "utf-8"));
@@ -104,8 +26,8 @@ exports.generate = () => {
         const lastNames = names.last;
         const lName = lastNames[Math.floor(Math.random() * lastNames.length)];
 
-        let data = getData(names, simCount, lName, gender, shift);
-        data += getMeta(simCount, names.first, lName);
+        let data = dataGetters.getData(names, simCount, lName, gender, shift);
+        data += dataGetters.getMeta(simCount, names.first, lName);
 
         stream.write(config.getRequestSampler(data));
       }
